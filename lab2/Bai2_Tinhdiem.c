c1bf8ff0b7db7878ae0c4131a065029ca6a431c5
hotdamngirl <cuongdiemphuc@gmail.com>
2026-07-17 23:02:36 +0700

    hoanthanhbai2_lap2

---
+++ lab2/Bai2_Tinhdiem.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+float Toan, Ly, Hoa;
+float DiemTrungBinh;
+printf("nhap diem Toan, Ly, Hoa:");
+scanf("%f %f %f", &Toan,&Ly,&Hoa);
+DiemTrungBinh = ( Toan*3+Ly*2+Hoa)/6;
+printf("Diem Trung Binh la: %.2f\n", DiemTrungBinh);
+
+    }
+
+
